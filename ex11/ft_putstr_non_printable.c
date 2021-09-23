b49abc987ef9cfc76f80d678d01defefb23f23c4
Afonso Fernandes <afonsoteresofernandes@gmail.com>
2021-09-23 08:10:40 +0100

    Update ft_putstr_non_printable.c

--- ex11/ft_putstr_non_printable.c
+++ ex11/ft_putstr_non_printable.c
@@ -2,13 +2,18 @@
 
 void    ft_putstr_non_printable(char    *str)
 {
+    
     while (*str != '\0')
     {
         if (0 < *str < 31 || str == 127)
         {
             write(1, "\\", 1);
-            write(1, (*str / 16), 1);
-            write(1, *str % 16, 1);
+            write(1, (*str / 16) + 48, 1);
+            if ((*str % 16) >= 10)
+            {
+                *str = (*str % 16) + 55;
+                write(1, *str, 1);
+            }
         }
         str++;
     } 
